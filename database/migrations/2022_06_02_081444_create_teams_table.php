decc52e1d914d3fb60a7071e1c5c35daeae8e9da
Ryan Hartadi <Ryanhartadi06@gmail.com>
2022-06-02 15:28:23 +0700

    initial database

---
+++ database/migrations/2022_06_02_081444_create_teams_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('image')->nullable();
+            $table->string('name')->nullable();
+            $table->string('github')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('gmail')->nullable();
+            $table->string('instagram')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('contacts')->insert([
+            'address' => 'Jl. Arjasa',
+            'phone_number' => '089271623',
+            'email' => 'milla@gmail.com',
+            'instagram' => 'milla@gmail.com',
+            'url' => 'milla@gmail.com',
+        ]);
+    }
+}
